2dbc1c9271afb349136391bf04c6cffa11c13ca5
SlavikLukiv <32965034+SlavikLukiv@users.noreply.github.com>
2020-10-14 14:05:07 +0300

    Update main.py

--- Cezar task/main.py
+++ Cezar task/main.py
@@ -1,87 +1,37 @@
-alphabetUpper = 'АБВГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ'
-alphabetLower = 'абвгдеєжзиіїйклмнопрстуфхцчшщьюя'
+ukr_alf = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя"
 
-def encode(text: str, key: int) -> str:
-    encodedAlphabetUpper = 'АБВГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ'
-    encodedAlphabetLower = 'абвгдеєжзиіїйклмнопрстуфхцчшщьюя'
-
-
-    encodedAlphabetUpper = encodedAlphabetUpper[key:] + encodedAlphabetUpper[:key]
-    encodedAlphabetLower = encodedAlphabetLower[key:] + encodedAlphabetLower[:key]
-
-    x = 0
-    encodedText = ''
-
-    while(x < len(text)):
-        y = 0
-        if text[x].isupper():
-            while (y < len(alphabetUpper)):
-                if text[x] == alphabetUpper[y]:
-                    encodedText += encodedAlphabetUpper[y]
-                y+=1
-        elif text[x].islower():
-            while (y < len(alphabetLower)):
-                if text[x] == alphabetLower[y]:
-                    encodedText += encodedAlphabetLower[y]
-                y += 1
+encode_e = ""
+def encrypt(text, key):
+    global encode_e
+    for letter in text:
+        if letter in ukr_alf:
+            t = ukr_alf.find(letter)
+            new_key: int = (t + key) / len(ukr_alf)
+            encode_e += ukr_alf[new_key]
         else:
-            if text[x] == ' ':
-                encodedText += ' '
-            elif text[x] == '!':
-                encodedText += '!'
-            elif text[x] == ',':
-                encodedText += ','
-            else:
-                encodedText += '.'
-        x+=1
+            encode_e += letter
+    return encode_e
 
-    text = encodedText
-    print(text)
-    return text
-
-def decode(text: str, key: int) -> str:
-    encodedAlphabetUpper = 'АБВГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ'
-    encodedAlphabetLower = 'абвгдеєжзиіїйклмнопрстуфхцчшщьюя'
-
-    encodedAlphabetUpper = encodedAlphabetUpper[key:] + encodedAlphabetUpper[:key]
-    encodedAlphabetLower = encodedAlphabetLower[key:] + encodedAlphabetLower[:key]
-
-    x = 0
-    decodedText = ''
-
-    while (x < len(text)):
-        y = 0
-        if text[x].isupper():
-            while (y < len(alphabetUpper)):
-                if text[x] == encodedAlphabetUpper[y]:
-                    decodedText += alphabetUpper[y]
-                y += 1
-        elif text[x].islower():
-            while (y < len(encodedAlphabetLower)):
-                if text[x] == encodedAlphabetLower[y]:
-                    decodedText += alphabetLower[y]
-                y += 1
+encode_d = ""
+def decipher(text, key):
+    global encode_d
+    for letter in text:
+        if letter in ukr_alf:
+            t = ukr_alf.find(letter)
+            new_key = (t - key) / len(ukr_alf)
+            encode_d += ukr_alf[new_key]
         else:
-            if text[x] == ' ':
-                decodedText += ' '
-            elif text[x] == '!':
-                decodedText += '!'
-            elif text[x] == ',':
-                decodedText += ','
-            else:
-                decodedText += '.'
-        x += 1
-    text = decodedText
-    print(text)
-    return text
-if __name__ == '__main__':
+            encode_d += letter
+        return encode_d
 
-    text_to_encode = 'Привіт. Сьогодні чудовий день для нападу на галлів'
-    key = 8
-    encoded_text = encode(text_to_encode, key)
-    decoded_text = decode(encoded_text, key)
-
-    assert text_to_encode == decoded_text, "Oooops! Something go wrong"
-
-
+print("Enter 'Е' to encrypt, or 'Д' to decipher")
+choice = input("")
+text = input("Plain Text : ")
+key = int(input("Cipher key : "))
+if choice == "Е" or "е":
+    encrypt(text, key)
+    print("Cipher: ", encode_e)
+elif choice == "Д" or "д":
+    decipher(text, key)
+    print("Cipher: ", encode_d)
 
